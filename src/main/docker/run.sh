a628ec9f8f0f194d8aa0b3b406a4029516f97539
Thimoty Barbieri <thimoty@thimoty.it>
2021-07-18 18:02:31 +0200

    mancava virgoletta

--- src/main/docker/run.sh
+++ src/main/docker/run.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
 echo "********************************************************"
-echo "Starting Saleforce Server 
+echo "Starting Saleforce Server "
 echo "********************************************************"
 java -Dspring.profiles.active=$PROFILE -jar /usr/local/salesforceservice/@project.build.finalName@.jar
